1af0180e3ea03c40107bd8fee4c032512ff1a688
Ibrahim Afzal Shareef <ibrahim.afzal1999@gmail.com>
2025-12-06 18:22:57 +0800

    change clipboard

--- init.lua
+++ init.lua
@@ -33,16 +33,16 @@ vim.o.splitright = true
 vim.o.background = "dark"
 
 vim.g.clipboard = {
-    name = 'win32yank',
-    copy = {
-        ['*'] = 'clip.exe',
-        ['+'] = 'clip.exe',
-    },
-    paste = {
-        ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-        ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    },
-    cache_enabled = 0,
+  name = 'win32yank',
+  copy = {
+    ['+'] = 'win32yank.exe -i --crlf',
+    ['*'] = 'win32yank.exe -i --crlf',
+  },
+  paste = {
+    ['+'] = 'win32yank.exe -o --lf',
+    ['*'] = 'win32yank.exe -o --lf',
+  },
+  cache_enabled = 0,
 }
 
 vim.cmd("filetype plugin on")
